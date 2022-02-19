2ec8d6a84e78eef362cab9c11a8229ddb5a5de01
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-02-19 07:33:48 +0000

    uh

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
   bg:"https://image.freepik.com/free-vector/background-design-with-blue-black-theme_1308-4821.jpg",//background image of all pages
   icon:"https://cdn.discordapp.com/avatars/889500207224586311/e8d0a4d4746f08fce4ab9fb4283f9edb.png?size=100",//bot icon url
   token:process.env.token,//bot token
-  redirect:"https://UsefulMonumentalEmbed.dreamy-dankers.repl.co/callback",//bot redirect url in this case: <your url>/callback
+  redirect:"<your dash url>/callback",//bot redirect url in this case: <your url>/callback
   secret:process.env.s//bot secret
 
 }
